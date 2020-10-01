efa18f44208c01800c572a3d66a31dcbcefd3912
beni2412 <hardaman97@gmail.com>
2020-10-01 22:24:32 +0530

    [Hardaman] Added: Test cases for happy and sad messages

--- src/main/java/com/capg/junittesting/MoodAnalyser.java
+++ src/main/java/com/capg/junittesting/MoodAnalyser.java
@@ -5,5 +5,13 @@ public class MoodAnalyser {
 	public static void main(String[] args) {
 		System.out.println("Welcome to mood analyser");
 	}
-}
 	
+	public String analyseMood(String msg) {
+		if(msg.contains("sad"))
+				return "SAD";
+		else
+			return "HAPPY";
+		
+	}
+
+}
--- src/test/java/JUnitTesting/MoodAnalyserTest.java
+++ src/test/java/JUnitTesting/MoodAnalyserTest.java
@@ -6,6 +6,15 @@ public class MoodAnalyserTest {
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad()
 	{
-		
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("This is a sad message");
+		Assert.assertEquals("SAD", mood);
+	}
+	@Test
+	public void givenMessage_WhenNotSad_ShouldReturnHappy()
+	{
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("This is Happy message");
+		Assert.assertEquals("HAPPY", mood);
 	}
 }
