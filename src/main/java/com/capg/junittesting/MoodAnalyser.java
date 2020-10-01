1465a624fbd6c5002739413f53ead7433c04f169
beni2412 <hardaman97@gmail.com>
2020-10-01 22:21:41 +0530

    [Hardaman] Added: Welcome message

---
+++ src/main/java/com/capg/junittesting/MoodAnalyser.java
@@ -0,0 +1,9 @@
+package com.capg.junittesting;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to mood analyser");
+	}
+}
+	
---
+++ src/test/java/JUnitTesting/MoodAnalyserTest.java
@@ -0,0 +1,11 @@
+package JUnitTesting;
+import com.capg.junittesting.*;
+import org.junit.Test;
+import org.junit.Assert;
+public class MoodAnalyserTest {
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad()
+	{
+		
+	}
+}
