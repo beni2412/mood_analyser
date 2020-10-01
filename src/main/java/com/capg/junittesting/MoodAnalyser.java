74496bd31c6206e7418fde65fc5951a78f890617
beni2412 <hardaman97@gmail.com>
2020-10-01 22:32:23 +0530

    [Hardaman] Refactored: taking the code to take the mood message in constructor

--- src/main/java/com/capg/junittesting/MoodAnalyser.java
+++ src/main/java/com/capg/junittesting/MoodAnalyser.java
@@ -1,12 +1,20 @@
 package com.capg.junittesting;
 
 public class MoodAnalyser {
+	private String msg;
+	
+	public MoodAnalyser() {
+	}
+	
+	public MoodAnalyser(String msg) {
+		this.msg =msg;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to mood analyser");
 	}
 	
-	public String analyseMood(String msg) {
+	public String analyseMood() {
 		if(msg.contains("sad"))
 				return "SAD";
 		else
